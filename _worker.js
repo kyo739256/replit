7203af5553885c9a8df15f8f34909b6f6baeb5cb
kyo739256 <130207293+kyo739256@users.noreply.github.com>
2023-08-02 21:13:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'fuzhi.li8430567.repl.co'
+      url.hostname = 'series-v1.samanehha.co'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
